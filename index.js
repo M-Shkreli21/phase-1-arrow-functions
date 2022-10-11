b2ac0fcb4fcb6eb1092d38a6efac0967d2e1f6c3
Mark Shkreli <m.shkreli96@icloud.com>
2022-10-11 19:16:19 -0400

    commit tree 07ffc53be9318b009821f1e1a32cb1ff2e90d534

---
+++ index.js
@@ -0,0 +1,23 @@
+let num1;
+let num2;
+let results;
+
+function divide(num1, num2){
+    results = num1 / num2
+    return results
+}
+
+console.log(2000, 100)
+
+let x;
+
+const square = (x) => x ** x;
+
+console.log(square(2));
+
+let a;
+let b;
+
+const add = (a, b) => a + b;
+
+console.log(add(3, 4));
